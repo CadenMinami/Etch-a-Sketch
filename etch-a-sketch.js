f882ce521fa224d73da5a0715e53c1ea4ad4cbf6
Caden Minami <cadenminami@gmail.com>
2025-08-28 21:33:58 +0900

    added a grid adjustor button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,57 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const divContainer = document.getElementById("container");
-    const gridSize = 16;
-    for (let j = 0; j < gridSize; j++) {
-        const divColumn = document.createElement("div");
-        divColumn.id = "column";
-        divContainer.append(divColumn);
-        for (let i = 0; i < gridSize; i++) {
-            const divBox = document.createElement("div");
-            divBox.classList.add("divChild");
-            divBox.textContent = "I am div " + (i.toString())
-            divColumn.appendChild(divBox);
+    const GRIDSIZE = 16;
+    createGrid(GRIDSIZE);
+
+
+    //Adding the hover functionality of the etch-a-sketch
+    const hoveritem = document.querySelectorAll("div.divChild");
+
+    hoveritem.forEach(Element => {
+        Element.addEventListener("mouseover", applyHover)
+        Element.addEventListener("mouseout", removeHover)
+    })
+    function createGrid(gridSize) {
+        divContainer.innerHTML = "";
+        for (let j = 0; j < gridSize; j++) {
+            const divColumn = document.createElement("div");
+            divColumn.classList.add("column");
+            divContainer.append(divColumn);
+            for (let i = 0; i < gridSize; i++) {
+                const divBox = document.createElement("div");
+                divBox.classList.add("divChild");
+                divBox.addEventListener("mouseover", applyHover);
+                divBox.addEventListener("mouseout", removeHover);
+                divColumn.appendChild(divBox);
+            }
         }
+
     }
+
+
+    function applyHover(event) {
+        event.target.style.backgroundColor = "red";
+    }
+
+    function removeHover(event) {
+        event.target.style.backgroundColor = "";
+    }
+    const gridSizeAdjustorBtn = document.getElementById("gridSize");
+    gridSizeAdjustorBtn.addEventListener("click", () => {
+        let userInput = prompt("Enter desired amount of squares per side: (Max 100)")
+        let size = parseInt(userInput);
+        if (isNaN(size) || size <= 0) {
+            alert("Please enter a valid number!");
+            return;
+        }
+        if (size > 100) {
+            alert("Maximum allowed is 100!");
+            return;
+        }
+
+        createGrid(size);
+    });
+
 });
